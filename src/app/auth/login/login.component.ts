19cd6929e9c11ac2aeec2fed9e24391050dd0943
12Balu34 <bastian.scherber@gmail.com>
2020-09-15 17:58:21 +0200

    Hide Login Button if user has valid id and access tokens

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ngx-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent  {
+export class LoginComponent implements OnInit {
+  isValidIdToken: boolean;
+  isValidAccessToken: boolean;
 
-  constructor(private oAuthService: OAuthService) {
+  constructor(private oAuthService: OAuthService, private router: Router) {
+    this.isValidAccessToken = this.oAuthService.hasValidAccessToken();
+    this.isValidIdToken = this.oAuthService.hasValidIdToken();
+  }
+
+  ngOnInit(): void {
+    if ( this.isValidAccessToken && this.isValidIdToken ) {
+      this.router.navigateByUrl('/pages/overview');
+    }
   }
 
   login() {
